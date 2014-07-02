aab3f7c79e27d136a0674d3e55f427822edfe234
zhalnin <zhalninreg@me.com>
2014-07-02 11:54:33 +0400

    Main script - shop
    Главная страница платежки

---
+++ gamelist/views/layouts/shop.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhalnin
+ * Date: 01/07/14
+ * Time: 15:13
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gamelist</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>Gamelist</h1>
+
+<div class="cart">
+    <p><b>Shopping cart</b></p>
+    <p><?php print_r( $_SESSION['cart']); ?></p>
+    <p><?php echo $_SESSION['total_items']; ?> items</p>
+    <p><?php echo number_format( $_SESSION['total_price'], 2); ?> rub</p>
+    <p><a href="index.php?view=checkout" >checkout</a></p>
+</div>
+
+<hr />
+
+<?php include( $_SERVER['DOCUMENT_ROOT'].'/'.'service/gamelist/views/'.$controller.'/'.$view.'.php' );  ?>
+
+</body>
+</html>
